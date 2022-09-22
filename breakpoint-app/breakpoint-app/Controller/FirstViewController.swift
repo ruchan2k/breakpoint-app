c4b1e4ae4b2b3f5ac3b5e1e3a1e6ccb3ca01356e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-09-22 09:41:45 +0300

    DataService is created folders are created as organised and firebase is implemented to project

---
+++ breakpoint-app/breakpoint-app/Controller/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  breakpoint-app
+//
+//  Created by Ruçhan Talha Günay on 21.09.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
